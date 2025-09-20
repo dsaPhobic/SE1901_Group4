38a033b1aeb628c8fa51c1708d241be65b243e0b
quan23 <xuanwc321@gmail.com>
2025-09-20 12:31:44 +0700

    create database.

---
+++ WebAPI/WebAPI/Models/ExamAttempt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPI.Models;
+
+public partial class ExamAttempt
+{
+    public long AttemptId { get; set; }
+
+    public int ExamId { get; set; }
+
+    public int UserId { get; set; }
+
+    public DateTime StartedAt { get; set; }
+
+    public DateTime? SubmittedAt { get; set; }
+
+    public virtual Exam Exam { get; set; } = null!;
+
+    public virtual ICollection<ExamAnswer> ExamAnswers { get; set; } = new List<ExamAnswer>();
+
+    public virtual User User { get; set; } = null!;
+}
---
+++ WebAPI/WebAPI/Models/Reading.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPI.Models;
+
+public partial class Reading
+{
+    public int ReadingId { get; set; }
+
+    public string ReadingContent { get; set; } = null!;
+
+    public string ReadingQuestion { get; set; } = null!;
+
+    public string? ReadingType { get; set; }
+}
