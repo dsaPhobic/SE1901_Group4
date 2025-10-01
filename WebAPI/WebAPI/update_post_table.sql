cf55384f8fc8be43d373af6f3e74b3242e320d70
CodeCoward <hmquang9805@gmail.com>
2025-10-01 09:14:08 +0700

    Delete WebAPI/WebAPI/update_post_table.sql

--- WebAPI/WebAPI/update_post_table.sql
+++
@@ -1,7 +0,0 @@
--- Add Title and ViewCount columns to Post table
-ALTER TABLE Post ADD Title NVARCHAR(200) NOT NULL DEFAULT 'Untitled';
-ALTER TABLE Post ADD UpdatedAt DATETIME2(0) NULL;
-ALTER TABLE Post ADD ViewCount INT NULL DEFAULT 0;
-
--- Update existing posts to have a default title
-UPDATE Post SET Title = 'Question about ' + CAST(PostId AS NVARCHAR(10)) WHERE Title = 'Untitled';
